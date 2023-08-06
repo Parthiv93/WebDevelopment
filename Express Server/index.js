9a034d49229a8f82f0ccedfdade3db42af35033a
Parthiv93 <parthivmanoj93@gmail.com>
2023-08-06 15:58:45 +0530

    Band Name Generator

--- Express Server/index.js
+++ Express Server/index.js
@@ -1,19 +1,28 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app=express();
 const port=3000;
+var bandName= "";
 
-app.get("/",(req,res)=>{
-    res.send("<h1>Home Page</h1>");
+app.use(express.urlencoded({ extended: true}));
+
+function bandNameGenerator(req, res, next){
+    console.log(req.body);
+    bandName=req.body["street"] + req.body["pet"];
+    next();
+}
+
+app.use(bandNameGenerator);
+
+app.get("/",(req,res)=> {
+    res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/about",(req,res)=>{
-    res.send("<h1>About Page</h1><p>My name is Parthiv Manoj</p>");
+app.post("/submit", (req, res)=> {
+   res.send(`<h1>Your band name is  : </h1><h2>${bandName}</h2>`);
 });
-
-app.get("/contact",(req,res)=>{
-    res.send("<h1>Contact Details</h1><p>Email : parthivmanoj15@gmail.com</p>");
-});
-
 app.listen(port,()=>{
     console.log(`Server running at port ${port}.`);
-})
+});
